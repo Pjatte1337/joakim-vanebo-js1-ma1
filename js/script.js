084ceed112a2b75ece9ec085ca4148439468da51
Pjatte1337 <Joakimvanebo1@gmail.com>
2022-01-29 20:25:30 +0100

    Q7

---
+++ js/script.js
@@ -0,0 +1,63 @@
+//Question 1:
+let cat = {
+    complain: function () {
+        console.log("Meow!");
+    },
+};
+
+
+//Question 2:
+const heading = document.querySelector("h3");
+heading.innerHTML = "Updated heading";
+
+
+//Question 3:
+heading.style.fontSize = "2em";
+
+
+//Question 4:
+heading.className = "subheading";
+
+
+//Question 5:
+const paragraphs = document.querySelectorAll("p");
+
+console.dir(paragraphs);
+
+for(let i = 0; i < paragraphs.length; i++) {
+    console.dir(paragraphs[i].innerHTML);
+    paragraphs[i].style.color = "red";
+}
+
+
+//Question 6:
+const resultContainer = document.querySelector(".results");
+
+
+resultContainer.innerHTML = "<p>New paragraph</p>";
+resultContainer.style.backgroundColor = "yellow" 
+
+
+//Question 7:
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+function listElement(list){
+    for (var i = 0; i < cats.length; i++){
+        let catName = cats[i].name;
+        console.log(catName);
+    }
+}
+
+listElement();
